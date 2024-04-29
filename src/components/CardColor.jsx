fed5977869cca9fc1cdfb731233b7cac0995c2f8
Prado Nicolas <nicolas88prado@gmail.com>
2024-04-29 00:23:12 -0300

    agregue la logica para poder agregar/guardar y borrar el color ingresado

--- src/components/CardColor.jsx
+++ src/components/CardColor.jsx
@@ -1,39 +1,95 @@
 import CardSmall from './CardSmall';
-//import { Toast } from "primereact/toast";
+import { Toast } from "primereact/toast";
 import { Button, Form } from "react-bootstrap";
+import { useState, useRef, useEffect } from "react";
 import "../style/cardColor.css"
 
 const CardColor = () => {
+
+    const [color, setColor] = useState("white");
+    const [inputColor, setInputColor] = useState("");
+    const [colorSave, setColorSave] = useState(
+      JSON.parse(localStorage.getItem("coloresKey")) || []
+    );
+    const toast = useRef(null);
+
+    const showRules = () => {
+      toast.current.show({
+        severity: "warn",
+        summary: "Reglas",
+        detail: "Puede ingresar colores fijos o hexadecimales.",
+        life: 3000,
+      });
+    };
+
+    const showSucces = () => {
+      toast.current.show({
+        severity: "success",
+        summary: "Color agregado",
+        detail: "Color agregado correctamente.",
+        life: 3000,
+      });
+    };
+
+    const onEnterPress = () => {
+      if (inputColor.trim().length > 0) {
+        let arrayAux = [...colorSave];
+        setColor(inputColor);
+        let auxEncontrado = arrayAux.filter((elm) => elm == inputColor);
+        if (auxEncontrado.length === 0) {
+          arrayAux.push(inputColor.trim());
+          showSucces();
+        } else {
+          alert("color repetido");
+        }
+        setColorSave(arrayAux);
+      } else {
+        showRules();
+      }
+    };
+
+    useEffect(() => {
+      localStorage.setItem("coloresKey", JSON.stringify(colorSave));
+    }, [colorSave]);
+
+    const deleteColor = (nombre) => {
+      let aux = [...colorSave].filter((elm) => elm !== nombre);
+      setColorSave(aux);
+    };
+
     return (
-        <div className="container">
-      <div className="principalContainer p-0 py-3">
-        <h3 className="px-4 m-0 mb-3">Administrar colores</h3>
-        {/* alerta/aviso */}
-        <div className="infoContainer py-4 d-flex align-items-center justify-content-center">
-          <div className="colorBlock"></div>
-          <Form.Group className="mx-5">
-            <Form.Control
-              type="text"
-              placeholder="Ingrese un color"
-            />
-            <div className="d-flex justify-content-end">
-              <Button
-                className="mt-3 me-3"
-                variant="primary"
-                type="button"
-                //onClick={}
-              >
-                Guardar
-              </Button>
-            </div>
-          </Form.Group>
+      <div className="container">
+        <div className="principalContainer p-0 py-3">
+          <h3 className="px-4 m-0 mb-3">Administrar colores</h3>
+          <Toast ref={toast} />
+          <div className="infoContainer py-4 d-flex align-items-center justify-content-center">
+            <div className="colorBlock" style={{ backgroundColor: color }}></div>
+            <Form.Group className="mx-5">
+              <Form.Control
+                type="text"
+                placeholder="Ingrese un color"
+                onChange={(event) => setInputColor(event.target.value)}
+              />
+              <div className="d-flex justify-content-end">
+                <Button
+                  className="mt-3 me-3 btn-dark"
+                  variant="primary"
+                  type="button"
+                  onClick={onEnterPress}
+                >
+                  Guardar
+                </Button>
+              </div>
+            </Form.Group>
+          </div>
+        </div>
+        <div className="row m-0">
+          {colorSave.map((elm, id) => (
+            <CardSmall colorNombre={elm} key={id} deleteColor={deleteColor}/>
+          ))}
         </div>
       </div>
-      <div className="row m-0">
-        <CardSmall></CardSmall>
-      </div>
-    </div>
     );
-};
+  };
 
 export default CardColor;
--- src/components/CardSmall.jsx
+++ src/components/CardSmall.jsx
@@ -1,7 +1,7 @@
 import { Button } from "react-bootstrap";
 import "../style/cardSmall.css";
 
-const CardSmall = () => {
+const CardSmall = ({ colorNombre, deleteColor }) => {
   return (
     <div className="col-lg-4 col-md-6 p-0">
       <div className="cardSmallContainer m-4 py-2">
@@ -9,14 +9,14 @@ const CardSmall = () => {
         <div className="d-flex justify-content-center">
           <div
             className="colorBlock"
-            //style={{ backgroundColor: }}
+            style={{ backgroundColor: colorNombre}}
           ></div>
         </div>
         <div className="d-flex justify-content-end">
           <Button
             className="mt-3 me-3"
             variant="danger"
-            //onClick={}
+            onClick={() => deleteColor(colorNombre)}
           >
             Borrar
           </Button>
